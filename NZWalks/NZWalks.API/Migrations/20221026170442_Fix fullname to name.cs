6884ef572985ed6fc89141f895e7691eb34401b2
Satya Priya Arya <satyapriyaarya@gmail.com>
2022-10-26 22:39:28 +0530

    with db context

---
+++ NZWalks/NZWalks.API/Migrations/20221026170442_Fix fullname to name.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    public partial class Fixfullnametoname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Walks",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Walks",
+                newName: "FullName");
+        }
+    }
+}
